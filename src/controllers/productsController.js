6f3df4235421e34225bc32af4a8fdbe26276df3a
Vittor Rafael <rafaelsales202205@gmail.com>
2024-03-25 21:08:53 -0300

    finalizando tabela de produtos

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,17 +2,21 @@ const knex = require('../config/connect');
 
 const addProduct = async (req, res) => {
   const { nome, descricao } = req.body;
+  const valorprod = parseFloat(req.body.valorprod);
   const valormin = parseFloat(req.body.valormin);
-  const valorconsult = parseFloat(req.body.valorconsult);
-  if (!valormin || !valorconsult || !nome || !descricao)
+  const valormax = parseFloat(req.body.valormax);
+  if (!valorprod || !valormax || !valormin || !nome || !descricao)
     return res.status(400).json({ error: 'Preencha todos os campos!' });
 
   try {
-    const valortotal = valormin + valorconsult;
+    const valorconsult = valormin;
+    const valortotal = valorprod + valorconsult;
 
     const newProduct = {
       nome,
       descricao,
+      valorprod: valorprod.toFixed(2),
+      valormax: valormax.toFixed(2),
       valormin: valormin.toFixed(2),
       valorconsult: valorconsult.toFixed(2),
       valortotal: valortotal.toFixed(2),
@@ -46,32 +50,47 @@ const editProduct = async (req, res) => {
   const { id } = req.params;
   try {
     const product = await knex('produtos').where('id', id);
+    if (product.length === 0)
+      return res.status(404).json({ error: 'Produto não encontrado!' });
+
     const { nome, descricao } = req.body;
-    let valormin = req.body.valormin;
-    let valorconsult = req.body.valorconsult;
-    if (!valormin && !valorconsult && !nome && !descricao)
+    let valorprod = parseFloat(req.body.valorprod);
+    let valormin = parseFloat(req.body.valormin);
+    let valormax = parseFloat(req.body.valormax);
+
+    if (!valormin && !valormax && !valorprod && !nome && !descricao)
       return res.status(400).json({ error: 'Nenhuma alteração encontrada!' });
 
-    let valortotal = product[0].valortotal;
-    if (valormin) {
-      valortotal = valorconsult
-        ? parseFloat(valormin) + parseFloat(valorconsult)
-        : parseFloat(valormin) + parseFloat(product[0].valorconsult);
+    valorprod = req.body.valorprod ? valorprod : product[0].valorprod;
+    valormin = req.body.valormin ? valormin : product[0].valormin;
+    valormax = req.body.valormax ? valormax : product[0].valormax;
+
+    let valorconsult = parseFloat(product[0].valorconsult);
+
+    if (valorconsult < parseFloat(valormin)) {
+      valorconsult = parseFloat(valormin);
     }
-    console.log(valortotal);
-    valormin = valormin ?? product[0].valormin;
-    valorconsult = valorconsult ?? product[0].valorconsult;
+
+    if (valorconsult > parseFloat(valormax)) {
+      valorconsult = parseFloat(valormax);
+    }
+
+    let valortotal = parseFloat(valorprod) + valorconsult;
 
     const data = {
       nome: nome ?? product[0].nome,
       descricao: descricao ?? product[0].descricao,
+      valorprod: req.body.valorprod
+        ? parseFloat(valorprod).toFixed(2)
+        : product[0].valorprod,
       valormin: req.body.valormin
         ? parseFloat(valormin).toFixed(2)
         : product[0].valormin,
-      valorconsult: req.body.valorconsult
-        ? parseFloat(valorconsult).toFixed(2)
-        : product[0].valorconsult,
-      valortotal: req.body.valormin ? valortotal.toFixed(2) : valortotal,
+      valormax: req.body.valormax
+        ? parseFloat(valormax).toFixed(2)
+        : product[0].valormax,
+      valorconsult: valorconsult.toFixed(2),
+      valortotal: valortotal.toFixed(2),
     };
 
     await knex('produtos').where('id', id).update(data).returning('*');
@@ -87,6 +106,8 @@ const removeProduct = async (req, res) => {
 
   try {
     const productDeleted = await knex('produtos').del().where('id', id);
+    if (productDeleted === 0)
+      return res.status(404).json({ error: 'Produto não encontrado!' });
     if (productDeleted.rowCount === 0)
       return res.status(400).json({
         error: 'Não foi possível excluir o Produto, tente novamente!',
@@ -98,9 +119,48 @@ const removeProduct = async (req, res) => {
   }
 };
 
+const editValorConsult = async (req, res) => {
+  const { id } = req.params;
+  try {
+    if (!req.body.valorconsult)
+      return res.status(400).json({ error: 'Nenhuma alteração encontrada!' });
+
+    let valorconsult = parseFloat(req.body.valorconsult);
+
+    const product = await knex('produtos').where('id', id);
+    if (product.length === 0)
+      return res.status(404).json({ error: 'Produto não encontrado!' });
+
+    if (valorconsult > parseFloat(product[0].valormax))
+      return res.status(400).json({
+        error: 'O novo valor não pode ser maior que o valor máximo definido!',
+      });
+    if (valorconsult < parseFloat(product[0].valormin))
+      return res.status(400).json({
+        error: 'O novo valor não pode ser menor que o valor mínimo definido!',
+      });
+
+    let valorprod = parseFloat(product[0].valorprod);
+    let valortotal = parseFloat(valorprod) + valorconsult;
+
+    await knex('produtos')
+      .where('id', id)
+      .update({
+        valorconsult: valorconsult.toFixed(2),
+        valortotal: valortotal.toFixed(2),
+      })
+      .returning('*');
+
+    return res.status(200).json({ success: 'Valor atualizado com sucesso!' });
+  } catch (error) {
+    return res.status(500).json({ error: 'Erro no servidor!' });
+  }
+};
+
 module.exports = {
   addProduct,
   listProducts,
   editProduct,
   removeProduct,
+  editValorConsult,
 };
