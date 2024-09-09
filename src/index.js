8af8d720216b5b811b06f433651164a3a853215d
ES Alan Guerra <alandasilvaguerra@proton.me>
2024-09-08 21:38:12 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
   origin: cors_origin,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
